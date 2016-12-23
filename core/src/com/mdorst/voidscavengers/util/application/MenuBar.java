386249613607e3b335807dd6ff788fefd4ec7154
Michael Dorst <code@mikefdorst.com>
2016-12-22 16:08:57 -0800

    Add comment re apple menu bar settings

--- core/src/com/mdorst/voidscavengers/util/application/MenuBar.java
+++ core/src/com/mdorst/voidscavengers/util/application/MenuBar.java
@@ -2,6 +2,7 @@ package com.mdorst.voidscavengers.util.application;
 
 public class MenuBar {
   public static void setApplicationName(String name) {
+    // This sets the application name listed in the menu bar on a mac to Void Scavengers
     System.setProperty("com.apple.mrj.application.apple.menu.about.name", name);
   }
 }
